daab2915ad2e79b3a40ea71c1a9a74beeb625bae
Yann Leroy <contact.54@icloud.com>
2025-09-10 20:25:52 +0200

    refonte page forgot

--- pages/forgot-password.js
+++ pages/forgot-password.js
@@ -5,6 +5,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import Header from '../components/Header'; // AJOUT DE L'IMPORT DU HEADER
+import Footer from '../components/Footer'; // AJOUT DE L'IMPORT DU HEADER
 import styles from '../styles/forgot-password.module.css';
 
 export default function ForgotPassword() {
