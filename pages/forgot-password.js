679e24ead71f0fa571acb32bc0416a286a93c501
Yann Leroy <contact.54@icloud.com>
2025-09-10 20:47:18 +0200

    refonte page forgot

--- pages/forgot-password.js
+++ pages/forgot-password.js
@@ -91,7 +91,6 @@ export default function ForgotPassword() {
               <div className={styles.brandSection}>
                 <h2 className={styles.brandTitle}>MonSavonVert</h2>
               </div>
-            
           </div>
 
           {/* Colonne de droite - Formulaire */}
